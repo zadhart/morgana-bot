b9b3b4d79a3a7a5c889b1307e23c380a62e8a029
Wagner Anthony <59626222+zadhart@users.noreply.github.com>
2022-02-04 11:25:02 -0300

    New cristove

--- cristove.py
+++ cristove.py
@@ -1,4 +1,5 @@
 import praw
+import requests
 
 reddit = praw.Reddit(client_id ='I_8uP2iGKeEWBoK6ykC3uw',
                      client_secret ='r9u9Vsbf_U-rJxp_2bAI1ztQz1bREw',
@@ -11,3 +12,8 @@ subreddit = reddit.subreddit('wallpaper')
 for submission in reddit.subreddit('wallpaper').hot(limit=10):
 
     print(submission.url)
+
+    response = requests.get(submission.url)
+
+    with open(str(submission) + ".jpg", "wb") as f:
+        f.write(response.content)
